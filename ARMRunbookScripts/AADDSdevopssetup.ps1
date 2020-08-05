68cf16a56bc35d77eea344503f57c957c216168f
samvdjagt <52912346+samvdjagt@users.noreply.github.com>
2020-08-05 12:18:52 -0600

    Update AADDSdevopssetup.ps1

--- ARMRunbookScripts/AADDSdevopssetup.ps1
+++ ARMRunbookScripts/AADDSdevopssetup.ps1
@@ -225,7 +225,7 @@ $body = @"
 {
   "parameters": {
     "gitSource": {
-      "url": "https://github.com/samvdjagt/wvdquickstart.git"
+      "url": "https://github.com/stgeorgi/wvdquickstart.git"
     }
   }
 }
@@ -241,7 +241,6 @@ $domainName = $split[1]
 
 # In case Azure AD DS is used, create a new user here, and assign it to the targetGroup. The principalID of this group will then be used.
 if ($identityApproach -eq 'Azure AD DS') {
-  $targetGroup = 'AAD DC Administrators'
   $url = $($fileURI + "/Modules/ARM/UserCreation/Parameters/users.parameters.json")
   Invoke-WebRequest -Uri $url -OutFile "C:\users.parameters.json"
   $ConfigurationJson = Get-Content -Path "C:\users.parameters.json" -Raw -ErrorAction 'Stop'
