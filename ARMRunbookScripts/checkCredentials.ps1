75c2280cbe351a68265ce4047def8b21356fe40d
samvdjagt <52912346+samvdjagt@users.noreply.github.com>
2020-08-07 12:40:30 -0600

    Update checkCredentials.ps1

--- ARMRunbookScripts/checkCredentials.ps1
+++ ARMRunbookScripts/checkCredentials.ps1
@@ -4,6 +4,7 @@ $ResourceGroupName = Get-AutomationVariable -Name 'ResourceGroupName'
 $fileURI = Get-AutomationVariable -Name 'fileURI'
 $existingVnetName = Get-AutomationVariable -Name 'existingVnetName'
 $existingSubnetName = Get-AutomationVariable -Name 'existingSubnetName'
+$identityApproach = Get-AutomationVariable -Name 'identityApproach'
 
 # Download files required for this script from github ARMRunbookScripts/static folder
 $FileNames = "msft-wvd-saas-api.zip,msft-wvd-saas-web.zip,AzureModules.zip"
@@ -28,6 +29,34 @@ Import-Module AzureAD -Global
 Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope Process -Force -Confirm:$false
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine -Force -Confirm:$false
 Get-ExecutionPolicy -List
+
+# Region check Azure AD DS credentials and membership of AAD DC administrators group
+if ($identityApproach -eq 'Azure AD DS') {
+	#The name of the Automation Credential Asset this runbook will use to authenticate to Azure.
+	$domainCredentialsAsset = 'domainJoinCredentials'
+
+	#Authenticate Azure
+	#Get the credential with the above name from the Automation Asset store
+	$domainCredentials = Get-AutomationPSCredential -Name $domainCredentialsAsset
+
+	Write-Output "Azure AD DS is used, attempting Connect-AzureAD with the domain join credentials entered"
+	Try { 
+		Connect-AzureAD -Credential $domainCredentials
+	}
+	Catch {
+		Write-Error "Invalid domain join username or password entered - Connecting to Azure AD failed."
+		Throw 
+	}
+
+	$GroupObjectId = Get-AzureADGroup -Filter "DisplayName eq 'AAD DC Administrators'" | Select-Object ObjectId
+	$groupMember = Get-AzureADGroupMember -ObjectId $GroupObjectId.ObjectId | Where-Object {$_.UserPrincipalName -eq $domainCredentials.username}
+	if ($groupMember -eq $null) {
+		Write-Error "Entered domain join credentials correspond to a user that is not a member of the AAD DC Administrators group."
+		Throw 
+	}
+	Disconnect-AzureAD
+	Write-Output "Domain join user is a member of AAD DC administrators and the entered credentials are correct."
+}
 
 #The name of the Automation Credential Asset this runbook will use to authenticate to Azure.
 $AzCredentialsAsset = 'AzureCredentials'
