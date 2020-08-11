549aa37c5ad5e9b39d2b4d21f2a4133580f05dc5
samvdjagt <52912346+samvdjagt@users.noreply.github.com>
2020-08-11 12:14:15 -0400

    Update AADDScheckCredentials.ps1

--- ARMRunbookScripts/AADDScheckCredentials.ps1
+++ ARMRunbookScripts/AADDScheckCredentials.ps1
@@ -1,3 +1,5 @@
+# This version of the checkCredentials script is only used when starting with a new or empty Azure subscription.
+
 #Initializing variables from automation account
 $SubscriptionId = Get-AutomationVariable -Name 'subscriptionid'
 $ResourceGroupName = Get-AutomationVariable -Name 'ResourceGroupName'
